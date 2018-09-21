0a45f24db7ee74b5fec48e969bbab9f2e998d5c4
jeremyy97 <jeremy.cerdas97@gmail.com>
2018-09-21 09:54:28 -0600

    Add files via upload

---
+++ Semana2/Lab 2/Lab01.Console/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab1.Logic;
+
+namespace Lab01.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(">>>>>>>>>>>Person<<<<<<<<<<<<<");
+
+            ILogger logger = new Persona();  //INTERFAZ 
+
+            //EJEMPLO1
+            /*Persona person = new Persona();
+            person.Id = 1;
+            person.Name = "Eddie";
+            person.LastName = "Carranza";*/
+            
+            //EJEMPLO2
+            //Persona person = new Persona(1, "Eddie", "Carranza");
+
+            //EJEMPLO3
+            Persona person = new Persona { Id = 1, Name = "Eddie", LastName = "Carranza" };
+
+            string personString = string.Format("Id:{0}, Name: {1}, LastName: {2}",person.Id, person.Name, person.LastName);
+            System.Console.WriteLine(personString);
+            logger.Log("Hello");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ Semana2/Lab 2/Lab1.Console/Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Logic
+{
+    public class Persona : ILogger
+    {
+        public Persona()
+        {
+            
+        }
+
+        public Persona(int id, string name, string lastName)
+        {
+            Name = name;
+            Id = id;
+            LastName = lastName;
+        }
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string LastName { get; set; }
+
+        public void Log(string log)
+        {
+            System.Console.WriteLine("Something went wrong");
+        }
+    }
+}
